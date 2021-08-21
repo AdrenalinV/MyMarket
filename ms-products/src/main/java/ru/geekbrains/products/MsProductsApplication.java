df02a6a2ec92ecb9d923a25dc12654210a68b20c
Adrenalin <vadimtsh@gmail.com>
2021-08-21 14:47:25 +0300

    homeWork5_create

--- ms-products/src/main/java/ru/geekbrains/products/MsProductsApplication.java
+++ ms-products/src/main/java/ru/geekbrains/products/MsProductsApplication.java
@@ -2,10 +2,11 @@ package ru.geekbrains.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication(scanBasePackages = "ru.geekbrains")
+//@EnableEurekaClient
 public class MsProductsApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsProductsApplication.class, args);
